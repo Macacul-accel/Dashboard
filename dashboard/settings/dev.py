354ebfc8f9373f010a7adf8702cb8d1502b6b133
Leo LIU <leo_liu@hotmail.fr>
2024-10-18 17:24:46 +0200

    First commit

---
+++ dashboard/settings/dev.py
@@ -0,0 +1,9 @@
+from . import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'dashboard'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ dashboard_app/models.py
@@ -0,0 +1,19 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+#Store email verification token for user registration
+class EmailVerification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    verifaction_token = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    verified_at = models.BooleanField(default=False)
+
+    @property
+    def token_expiration_time(self):
+        expiration_time = timezone.timedelta(minutes=10)
+        return timezone.now() > self.created_at + expiration_time
